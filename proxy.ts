1a62a88bd1e42b9126a555f5a131483047ddaa7c
vinith <vinithdc@gmail.com>
2026-08-19 22:23:24 +0530

    Nav work 1

--- proxy.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server'
- 
-export function proxy(request: NextRequest) {
-  if (request.nextUrl.pathname === '/proxy-redirect') {
-    return NextResponse.redirect(new URL('/', request.url))
-  }
- 
-  return NextResponse.next()
-}
