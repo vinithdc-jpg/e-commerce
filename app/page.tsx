1a03984f3c5388217df05059c80d770f9f1552f9
vinith <vinithdc@gmail.com>
2026-08-20 17:17:58 +0530

    Hero work

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from '@/components/Hero'
+import NavBar from '@/components/NavBar'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
+    <>
+      <NavBar/>
       <Hero/>
-    </div>
+    </>
   )
 }
 
