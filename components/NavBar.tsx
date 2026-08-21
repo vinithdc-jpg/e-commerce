363645472099fc5a3d486c629412bfb519ed1acc
vinith <vinithdc@gmail.com>
2026-08-21 21:30:57 +0530

    htuA

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,50 +1,53 @@
+import Link from "next/link"
+
 const NavBar = () => {
   return (
     <div className="navbar bg-base-100 shadow-sm">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+          </div>
+          <ul
+            tabIndex={-1}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            <li><a>New arraval</a></li>
+            <li><a>Shop</a></li>
+            <li>
+              <a>Collection</a>
+              <ul className="p-2">
+                <li><a>Fassion</a></li>
+                <li><a>Classic</a></li>
+                <li><a>Ladise</a></li>
+              </ul>
+            </li>
+            <li><a>About</a></li>
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">LUMIÈRE</a>
       </div>
-      <ul
-       tabIndex={-1}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>New arraval</a></li>
-        <li><a>Shop</a></li>
-        <li>
-          <a>Collection</a>
-          <ul className="p-2">
-            <li><a>Fassion</a></li>
-            <li><a>Classic</a></li>
-            <li><a>Ladise</a></li>
-          </ul>
-        </li>
-        <li><a>About</a></li>
-      </ul>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li><a>New arraval</a></li>
+          <li><a>Shop</a></li>
+          <li>
+            <details>
+              <summary>Collection</summary>
+              <ul className="p-2 bg-base-100 w-40 z-1">
+                <li><a>Fassion</a></li>
+                <li><a>Classic</a></li>
+                <li><a>Ladise</a></li>
+              </ul>
+            </details>
+          </li>
+          <li><a>about</a></li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <Link href='/login'><button className="btn btn-soft btn-secondary">Login</button>
+        </Link>
+      </div>
     </div>
-    <a className="btn btn-ghost text-xl">LUMIÈRE</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>New arraval</a></li>
-        <li><a>Shop</a></li>
-      <li>
-        <details>
-          <summary>Collection</summary>
-          <ul className="p-2 bg-base-100 w-40 z-1">
-            <li><a>Fassion</a></li>
-            <li><a>Classic</a></li>
-            <li><a>Ladise</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>about</a></li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn">Log In</a>
-  </div>
-</div>
   )
 }
 
