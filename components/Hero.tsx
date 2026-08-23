ce7812e1fce2cb3088a05ce88019b094237627b4
vinith <vinithdc@gmail.com>
2026-08-23 11:40:14 +0530

    ui fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div className="hero  min-h-screen">
+    <div
+      className="hero min-h-screen bg-cover bg-center bg-no-repeat"
+      style={{
+        backgroundImage: "url('https://static.vecteezy.com/system/resources/thumbnails/011/911/023/small/full-street-fashion-slender-figure-beautiful-face-graceful-girl-sitting-on-isolated-white-background-portrait-of-female-model-in-studio-plastic-surgery-and-aesthetic-cosmetology-free-photo.jpg')",
+      }}
+    >
       <div className="hero-content text-center">
         <div className="max-w-md">
           <h1 className="text-8xl font-bold">THE NEW ELEGANCE</h1>
@@ -10,7 +15,7 @@ const Hero = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
