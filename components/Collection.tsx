9aa6655e3165ee356e41d339793c3d82d1813f9f
vinith <vinithdc@gmail.com>
2026-08-23 22:35:42 +0530

    CARD WORK

--- components/Collection.tsx
+++ components/Collection.tsx
@@ -1,10 +1,41 @@
 import React from "react";
 import Headline from "./ui/Headline";
 
+const cards = [
+  {
+    image:"/next.svg",
+    item:"TOP",
+    detail:"Structural Silk Blouse",
+    rate:"$122"
+  },
+  {
+    image:"/next.svg",
+    item:"BOTTOMS",
+    detail:"Tailored Wide-Leg Trouser",
+    rate:"$152"
+  },
+  {
+    image:"/next.svg",
+    item:"ACCESSORS",
+    detail:"Structured Leather Tote",
+    rate:"$766"
+  },
+  {
+    image:"/next.svg",
+    item:"Knitwear",
+    detail:"Lightweight Cashmere Knit",
+    rate:"$54"
+  },
+]
+
 const Collection = () => {
   return (
     <div>
       <Headline />
+
+      <div>
+
+      </div>
     </div>
   );
 };
