d2322e57ef62d9c912d818158767070a9d27341e
vinith <vinithdc@gmail.com>
2026-08-23 20:31:21 +0530

    Collection work

---
+++ components/ui/Headline.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Headline = () => {
+  return (
+    <div>
+      <div className='flex justify-between items-center min-w-screen'>
+        <h2>Our collections</h2>
+        <Link href="">view</Link>
+      </div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Headline
