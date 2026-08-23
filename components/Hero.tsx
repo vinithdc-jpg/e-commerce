aa9d7d0c46630819ebffcb0bee128b8ebfe88552
vinith <vinithdc@gmail.com>
2026-08-23 11:35:20 +0530

    UI contenct fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,15 +2,11 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className="hero bg-base-200 min-h-screen">
+    <div className="hero  min-h-screen">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Hello there</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-            quasi. In deleniti eaque aut repudiandae et a id nisi.
-          </p>
-          <button className="btn btn-primary">Get Started</button>
+          <h1 className="text-8xl font-bold">THE NEW ELEGANCE</h1>
+          <button className="btn btn-primary mt-5">Shop the Collection</button>
         </div>
       </div>
     </div>
