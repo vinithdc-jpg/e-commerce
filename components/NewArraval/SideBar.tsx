161bd4a9ddea55eccfa1746339642783d7250279
vinith <vinithdc@gmail.com>
2026-08-27 16:38:12 +0530

    sidebar

--- components/NewArraval/SideBar.tsx
+++ components/NewArraval/SideBar.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const SideBar = () => {
   return (
-    <div>
-      
+    <div className="drawer lg:drawer-open">
+      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col items-center justify-center">
+        {/* Page content here */}
+        <label htmlFor="my-drawer-3" className="btn drawer-button lg:hidden">
+          Open drawer
+        </label>
+      </div>
+      <div className="drawer-side">
+        <label
+          htmlFor="my-drawer-3"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <ul className="menu bg-base-200 min-h-full w-80 p-4">
+          {/* Sidebar content here */}
+          <li>
+            <a>Sidebar Item 1</a>
+          </li>
+          <li>
+            <a>Sidebar Item 2</a>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
