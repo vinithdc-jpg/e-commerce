18543c6528a04455d5357afeb44b85e93b98b3f4
vinith <vinithdc@gmail.com>
2026-09-01 09:16:09 +0530

    Todays first commit

--- app/Shop/page.tsx
+++ app/Shop/page.tsx
@@ -1,3 +1,4 @@
+import Collection from "@/components/Collection";
 import SideBar from "@/components/Shop/SideBar";
 import React from "react";
 
@@ -6,7 +7,10 @@ const page = () => {
     <div className="flex min-h-fit w-full">
       <SideBar />
       <div className="flex-1 px-6 py-8">
-        <h1 className="text-2xl font-semibold text-zinc-100 text-center">Shop All</h1>
+        <h1 className="text-2xl font-semibold text-zinc-100 text-center">
+          Shop All
+        </h1>
+        <Collection />
       </div>
     </div>
   );
